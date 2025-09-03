f67c5b3d099535422e77d8adb63f377eb4630b5e
Rebisco-Biscuit <23103808@usc.edu.ph>
2025-09-03 11:58:07 +0800

    completed LL var 1

--- ADTs/Linked Lists/variation_1.c
+++ ADTs/Linked Lists/variation_1.c
@@ -11,22 +11,37 @@ typedef struct {
     int count;
 } List;
 
+//create operations
 List* initialize();
 void insertFirst(List *list, int data);
 void insertLast(List *list, int data);
 void insertPos(List *list, int data, int index);
-void deleteStart(List *list);
-void deleteLast(List *lsit);
+
+//display operations
+int retrieve(List *list, int index);
+int locate(List *list, int data);
 void printList(List *list);
+
+//delete operations
+void empty(List *list);
 void deletePos(List *list, int index);
+void deleteLast(List *lsit);
+void deleteStart(List *list);
 
 int main() {
     List *L = initialize();
     insertFirst(L, 2);
     insertFirst(L, 1);
     insertLast(L, 3);
-    insertPos(L, 4, 3);
-    deletePos(L, 1);
+    empty(L);
+    // insertPos(L, 4, 3);
+    //deletePos(L, 2);
+    // printf("%d\n", retrieve(L, 3));
+    // printf("%d\n", locate(L, 1));
+    // printf("%d\n", locate(L, 2));
+    // printf("%d\n", locate(L, 3));   
+    // printList(L);
+    
     // insertPos(L, 5, 1);
     // deleteStart(L);
     //deleteLast(L);
@@ -34,31 +49,46 @@ int main() {
     return 0;
 }
 
-void deletePos(List *list, int index) {
-  if(index == 0) {
-    deleteStart(list);
-  } else if (index == list->count) {
-    deleteLast(list);
-  } else {
+//display operations
+int locate(List *list, int data) {
+  if(list->head == NULL){ return -1; }
+  else {
+    //int idx=0;
     Node *current = list->head;
-    for(int i=0; i<index-1; i++) {
+    for(int i=0; i<list->count-1; i++){
+      if(current->data == data) { return i; }
       current = current->next;
     }
-    Node *temp = current->next;
-    current->next = temp->next;
-    free(temp);
-    
   }
+  return -1;
 }
 
-List* initialize()  {
-    List *L = malloc(sizeof(List));
-    if(L == NULL) {
-        return NULL;
+int retrieve(List *list, int index) {
+  if(index < list->count-1) {
+    index -= 1;
+    Node *current = list->head;
+    for(int i=0; i<=index-1; i++) {
+      current = current->next;
     }
-    L->head = NULL;
-    L->count = 0;
-    return L;
+    return current->data;
+  }
+  return -1;
+}
+
+void printList(List *list) {
+  Node *current = list->head;
+  while(current!= NULL) {
+    printf(" %d ->", current->data);
+    current = current->next;
+  }
+  printf(" NULL");
+}
+
+//delete operations
+void empty(List *list) {
+  for(int i=list->count; i > 0; i--) {
+    deleteLast(list);
+  }
 }
 
 void deleteLast(List *list) {
@@ -85,6 +115,34 @@ void deleteStart(List *list) {
   }
 }
 
+void deletePos(List *list, int index) {
+  if(index == 0) {
+    deleteStart(list);
+  } else if (index == list->count) {
+    deleteLast(list);
+  } else {
+    Node *current = list->head;
+    for(int i=0; i<index-1; i++) {
+      current = current->next;
+    }
+    Node *temp = current->next;
+    current->next = temp->next;
+    free(temp);
+    
+  }
+}
+
+//create operations
+List* initialize()  {
+    List *L = malloc(sizeof(List));
+    if(L == NULL) {
+        return NULL;
+    }
+    L->head = NULL;
+    L->count = 0;
+    return L;
+}
+
 void insertPos(List *list, int data, int index) {
   if(index <= list->count) {
     if(index == list->count) { 
@@ -96,7 +154,7 @@ void insertPos(List *list, int data, int index) {
       newNode->data = data;
       newNode->next = NULL;
       Node *current = list->head;
-      for(int i=0; i<index-1; i++) { 
+      for(int i=0; i<index; i++) { 
         current = current->next; 
       }
       newNode->next = current->next;
